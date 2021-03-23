e2171ddf49d8fa8bfe4dbf00200a26ab19872be8
Violetta-Odagiu <violettaodagiu@gmail.com>
2021-03-24 00:45:40 +0200

    add homework1803

---
+++ src/homework1803/ArtimeticDemo.java
@@ -0,0 +1,23 @@
+package homework1803;
+
+public class ArtimeticDemo {
+        public static void main (String[] args) {
+            int result = 1 + 2; // 3;
+            System.out.println(result);
+
+            result = result - 1; // 2;
+            System.out.println(result);
+
+            result = result * 2; // 0;
+            System.out.println(result);
+
+            result = result / 2; // 0;
+            System.out.println(result);
+
+            result = result + 8; // 8;
+            result = result % 7; // 1'
+            System.out.println(result);
+
+        }
+
+}
